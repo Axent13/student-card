c908d5cbbc48c7b43d371ed08158826cacb9aa62
Axent13 <axent13@yandex.ru>
2022-10-30 10:36:25 +0700

    Age printing

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Modal = ({ isVisible }) => {
   return (
-    <div className={"modal" + (isVisible ? " d-block" : "")} tabindex="-1">
+    <div className={"modal" + (isVisible ? " d-block" : "")}>
       <div className="modal-dialog  modal-dialog-centered">
         <div className="modal-content">
           <div className="modal-body">
--- src/components/studentCard.jsx
+++ src/components/studentCard.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const StudentCard = ({ name, surname, yearOfBirth, portfolioUrl }) => {
+  const getAge = () => {
+    const age = Number(new Date().getFullYear()) - Number(yearOfBirth);
+    const lastNumber = Number(String(age).slice(-1));
+
+    if (age >= 5 && age <= 20) return `${age} лет`;
+    else if (lastNumber === 1) return `${age} год`;
+    else if (lastNumber >= 2 && lastNumber <= 4) return `${age} года`;
+    else return `${age} лет`;
+  };
+
   return (
     <div>
       {name ? (
@@ -21,7 +31,7 @@ const StudentCard = ({ name, surname, yearOfBirth, portfolioUrl }) => {
           {yearOfBirth ? (
             <p>
               <strong>Год рождения: </strong>
-              {yearOfBirth}
+              {yearOfBirth} ({getAge()})
             </p>
           ) : (
             ""
